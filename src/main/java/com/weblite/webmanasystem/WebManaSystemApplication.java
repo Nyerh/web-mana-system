5e5573e16af7d5403ab8e0f1cd75567058765822
nyerh <826618518@qq.com>
2020-02-27 16:00:21 +0800

    0227 update 1

--- src/main/java/com/weblite/webmanasystem/WebManaSystemApplication.java
+++ src/main/java/com/weblite/webmanasystem/WebManaSystemApplication.java
@@ -1,10 +1,8 @@
 package com.weblite.webmanasystem;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 public class WebManaSystemApplication {
